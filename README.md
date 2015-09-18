42535b912c16ea6af00a9352994ec00c503ec7ba
alex3683 <alexander.wilden@gmail.com>
2015-09-18 21:59:42 +0200

    Fixes and Firefox scrolling support

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # sliding-doors
 Simple Sliding Doors Effect
+
+Live example: [http://alex3683.github.io/sliding-doors/](http://alex3683.github.io/sliding-doors/)
--- sliding-doors.js
+++ sliding-doors.js
@@ -1,4 +1,4 @@
-var slidingDoors = (function() {
+function attachSlidingDoors() {
 
   var doors = document.querySelector('.doors');
   var button = document.querySelector('button');
@@ -6,28 +6,30 @@ var slidingDoors = (function() {
 
   button.addEventListener('click', toggleOpen);
 
-  var lastScrollTop = document.body.scrollTop;
-  document.body.onscroll = function(event) {
-    var newTop = document.body.scrollTop;
-    if (newTop > lastScrollTop) {
-      document.body.scrollTop = document.body.scrollHeight;
-      toggleOpen(true);
-    } else if (newTop < lastScrollTop) {
-      document.body.scrollTop = 0;
-      toggleOpen(false);
-    }
+  // Firefox scrolls on documentElement instead of body. So let's cover that with a feature test
+  document.body.scrollTop = 1;
+  var scrollElement = document.body.scrollTop === 1 ?
+    document.body : document.documentElement;
+
+  var lastScrollTop = scrollElement.scrollTop = 0;
+  document.body.onscroll = function() {
+    var newTop = scrollElement.scrollTop;
+    toggleOpen(newTop > lastScrollTop);
     lastScrollTop = newTop;
   };
 
-
   function toggleOpen(open) {
-    var isOpen = doors.classList.contains('open');
-    var setOpen = typeof open === 'boolean' ? open : !isOpen;
+    var setOpen = typeof open === 'boolean' ? open : !doors.classList.contains('open');
     if (setOpen) {
       doors.classList.add('open');
+      scrollElement.scrollTop = scrollElement.scrollHeight;
     } else {
       doors.classList.remove('open');
+      scrollElement.scrollTop = 0;
     }
   }
 
-})();
+
+}
+
+window.onload = attachSlidingDoors;
